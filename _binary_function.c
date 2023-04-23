dc67da9a1b87432cc2d9d09bdfbc60b79da5d9f1
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-23 11:44:36 -0700

    Update _binary_function.c

--- _binary_function.c
+++ _binary_function.c
@@ -1,54 +1,36 @@
 #include "main.h"
 
 /**
- * print_bin - Prints an unsigned int in binary format
- * @l: List of arguments
+ * print_bin - Prints an unsigned int in binary format.
+ * @l: List of arguments.
  *
- * Return: Number of binary digits printed
+ * Return: Number of binary digits printed.
  */
 int print_bin(va_list l)
 {
-	unsigned int n = va_arg(l, unsigned int), a;
+	unsigned int n = va_arg(l, unsigned int);
 	char c;
-	int ctr = 0, i = 0, retval;
-	char *binary;
-	
-	a = n;
-	
-	while (a > 0)
-	{
-		ctr++;
-		a /= 2;
-	}
-	
-	binary = malloc(sizeof(char) * (ctr + 1));
+	int ctr = 0, i = 0, j = 0, binary[32];
 
-	if (n < 1)
+	if (n == 0)
 	{
 		c = n + '0';
 		write(1, &c, 1);
 	}
 	else
 	{
-		if (binary == NULL)
-			return (-1);
-		for (i = 1; i < ctr + 1; i++)
+		while (n > 0)
 		{
-			binary[ctr - i] = n % 2;
+			binary[i] = n % 2;
 			n = n / 2;
+			ctr++;
+			i++;
 		}
-		for (i = 0; i < ctr; i++)
+		for (j = i - 1; j >= 0; j--)
 		{
-			c = binary[i] + '0';
-			retval = write(1, &c, 1);
-
-			if (retval == -1)
-			{
-				free(binary);
-				return (-1);
-			}
+			c = binary[j] + '0';
+			write(1, &c, 1);
 		}
 	}
-	free(binary);
 	return (ctr);
 }
