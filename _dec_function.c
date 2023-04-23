128690f5ddc849f5d0de8f7769aeb34e9ca8848a
riyo3350g <yechouben@gamail.com>
2023-04-23 15:53:59 +0000

    test 2

---
+++ _dec_function.c
@@ -0,0 +1,43 @@
+/**
+ *  * print_i - Prints an integer
+ *   * @l: List of arguments
+ *    *
+ *     * Return: Number of digits printed
+ *      */
+int print_i(va_list l)
+{
+		int ctr = 0, n;
+			int a = va_arg(l, int);
+				int countn = 1;
+					unsigned int number;
+						char c;
+
+							if (a < 0)
+									{
+												c = '-';
+														ctr += write(1, &c, 1);
+																number = a * -1;		
+																	}
+								else
+										{
+													number = a;
+														}
+									a = number;
+
+										while (number > 9)
+												{
+															number = number / 10;
+																	countn = countn * 10;
+																			ctr++;
+																				}
+
+											while (countn >= 1)
+													{
+																n = (a / countn) % 10;
+																		c = n + '0';
+																				write(1, &c, 1);
+																						countn = countn / 10;
+																							}
+
+												return ctr;
+}
