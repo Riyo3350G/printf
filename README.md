c05f77fb014722a63a5cad978636196c6423b00e
riyo3350g <yechouben@gamail.com>
2023-04-23 10:30:01 +0000

    README and header added

---
+++ README.md
@@ -0,0 +1,1 @@
+This Printf group Project
---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Function that produces output according to a format.
+ * @format: Format specifies
+ *
+ * Return: The number of characters printed.
+ * */
+int _printf(const char *format, ...)
+{
+	
+}
