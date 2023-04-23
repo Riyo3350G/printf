191057375bfa13a2d400a84a947e12891eade1eb
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-23 11:06:45 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -13,10 +13,10 @@ int _printf(const char *format, ...)
 	int counter = 0;
 
 	va_start(l, format);
-	
+
 	if (format == NULL || !format[i + 1])
 		return (-1);
-	
+
 	while (format[i])
 	{
 		if (format[i] == '%')
@@ -25,28 +25,23 @@ int _printf(const char *format, ...)
 			if (format[i])
 			{
 				if (format[i] == 'c')
-				{
 					counter += print_c(l);
-				}
 				else if (format[i] == 's')
-				{
 					counter += print_s(l);
-				}
 				else if (format[i] == 'd' || format[i] == 'i')
-				{
 					counter += print_i(l);
-				}
 				else if (format[i] == '%')
 				{
 					write(1, "%", 1);
 					counter++;
-				}else if (format[i] == 'b')
-				{
+				}
+				else if (format[i] == 'b')
 					counter += print_bin(l);
-				}else{
-					write(1, &format[i-1], 1);
-					write(1, &format[i],1);
-					counter = counter + 2;
+				else
+				{
+					write(1, &format[i - 1], 1);
+					write(1, &format[i], 1);
+					counter += 2;
 				}
 			}
 		}
@@ -59,5 +54,5 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(l);
-	return counter;
+	return (counter);
 }
