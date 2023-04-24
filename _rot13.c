18bb65ac7fcdb3901789813adfc0d9237b526c5d
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-24 09:40:09 -0700

    Update _rot13.c

--- _rot13.c
+++ _rot13.c
@@ -8,12 +8,10 @@
  */
 int print_rot13(va_list l)
 {
-	char upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-			'K', 'L', 'M', 'N', 'O', 'P',
-			'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-			'k', 'l', 'm', 'n', 'o', 'p', 'q',
-			'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M',
+			'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm',
+			'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int i, ctr = 0;
 
 	char *str = va_arg(l, char*);
@@ -26,7 +24,8 @@ int print_rot13(va_list l)
 
 	while (*str != '\0')
 	{
-		if (*str >= 'A' && *str <= 'Z') {
+		if (*str >= 'A' && *str <= 'Z')
+		{
 			for (i = 0; i < 26; i++)
 			{
 				if (upper[i] == *str)
@@ -35,7 +34,8 @@ int print_rot13(va_list l)
 			if ((i + 13) > 26)
 				i = (i + 13) % 26;
 			write(1, &upper[i], 1);
-		} else if (*str >= 'a' && *str <= 'z') {
+		} else if (*str >= 'a' && *str <= 'z')
+		{
 			for (i = 0; i < 26; i++)
 				if (lower[i] == *str)
 					break;
