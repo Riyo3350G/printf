cf9484afa45209ae616bb49fff418329ba005d24
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-24 08:01:09 -0700

    Update _rot13.c

--- _rot13.c
+++ _rot13.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_rot13 - Encodes a string using rot13
+ * @l: Argument list
+ *
+ * Return: Number of characters printed
+ */
 int print_rot13(va_list l)
 {
 	char upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
@@ -7,33 +13,32 @@ int print_rot13(va_list l)
 	int i, ctr = 0;
 
 	char *str = va_arg(l, char*);
-	while(*str != '\0')
+	while (*str != '\0')
 	{
-		if(*str >= 'A' && *str <= 'Z')
+		if (*str >= 'A' && *str <= 'Z')
 		{
-			for(i = 0; i < 26; i++)
+			for (i = 0; i < 26; i++)
 			{
-				if(upper[i] == *str)
-					break;	
-			}
-			i += 13;
-			if(i >= 26)
-				i = i % 26;
-			write(1, &upper[i], 1);
-		}else if(*str >= 'a' && *str <= 'z')
-		{
-			for(i = 0; i < 26; i++)
-			{
-				if(lower[i] == *str)
+				if (upper[i] == *str)
 					break;
 			}
 			i += 13;
-			if(i >= 26)
+			if (i > 26)
+				i = i % 26;
+			write(1, &upper[i], 1);
+		}
+		else if (*str >= 'a' && *str <= 'z')
+		{
+			for (i = 0; i < 26; i++)
+				if (lower[i] == *str)
+					break;
+			i += 13;
+			if (i > 26)
 				i = i % 26;
 			write(1, &lower[i], 1);
-		}else{
-			write(1, str, 1);
 		}
+		else
+			write(1, str, 1);
 		ctr++;
 		str++;
 	}
