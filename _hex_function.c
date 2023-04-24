61b359980f9f60eb276cda684cae66651a105685
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-24 05:09:57 -0700

    Update _hex_function.c

--- _hex_function.c
+++ _hex_function.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_bin - Prints an unsigned int in hexadecimal lower format.
+ * print_x - Prints an unsigned int in hexadecimal lower format.
  * @l: List of arguments.
  *
- * Return: Number of binary digits printed.
+ * Return: Number of hexadecimal digits printed.
  */
 int print_x(va_list l)
 {
-  unsigned int n = va_arg(l, unsigned int);
+  	unsigned int n = va_arg(l, unsigned int);
 	char c;
 	int ctr = 0, i = 0, j = 0, low_hex[32];
 
@@ -29,11 +29,54 @@ int print_x(va_list l)
 
 		for (j = i - 1; j >= 0; j--)
 		{
-      if(low_hex[j] > 9)
+		      if(low_hex[j] > 9)
+		      {
+			c = 'a' + (low_hex[j] % 10);
+		      }else{
+			c = low_hex[j] + '0';
+		      }
+					write(1, &c, 1);																	
+		}
+		return (ctr);
+	}
+}
+
+#include "main.h"
+
+/**
+ * print_X - Prints an unsigned int in hexadecimal upper format.
+ * @l: List of arguments.
+ *
+ * Return: Number of hexadecimal digits printed.
+ */
+int print_X(va_list l)
+{
+  unsigned int n = va_arg(l, unsigned int);
+	char c;
+	int ctr = 0, i = 0, j = 0, up_hex[32];
+
+	if (n == 0)
+	{
+		c = n + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		while (n > 0)
+		{
+			up_hex[i] = n % 16;																	
+			n = n / 16;
+			ctr++;
+			i++;
+		}
+
+		for (j = i - 1; j >= 0; j--)
+		{
+      if(up_hex[j] > 9)
       {
-        c = 'a' + (low_hex[j] % 10);
+        c = 'A' + (up_hex[j] % 10);
       }else{
-			  c = low_hex[j] + '0';
+			  c = up_hex[j] + '0';
       }
 			write(1, &c, 1);																	
 		}
