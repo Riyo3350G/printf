25716967e36ff8c2d24b2baaf0d810f8e6b6ea40
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-24 05:42:41 -0700

    Add unsigned

--- _dec_function.c
+++ _dec_function.c
@@ -44,3 +44,37 @@ int print_i(va_list l)
 
     return (ctr);
 }
+
+/**
+ * print_u - Prints an unsigned integer
+ * @l: List of arguments
+ *
+ * Return: Number of digits printed
+ */
+int print_u(va_list l)
+{
+    int ctr = 0, n;
+    unsigned int input = va_arg(l, unsigned int);
+    unsigned int countn = 1;
+    unsigned int number;
+    char c;
+
+    number = input;
+
+    while (number >= 10)
+    {
+        number /= 10;
+        countn *= 10;
+    }
+
+    while (countn >= 1)
+    {
+        n = (unsigned int) ((input / countn) % 10);
+        c = n + '0';
+        write(STDOUT_FILENO, &c, 1);
+        countn /= 10;
+        ctr++;
+    }
+
+    return (ctr);
+}
