d3c9c3a4b7f832b5c66ac4265e7d191aae14e271
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-25 01:12:41 -0700

    Update _rot13.c

--- _rot13.c
+++ _rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_rot13 - Encodes a string using rot13
  * @l: Argument list
@@ -7,44 +8,27 @@
  */
 int print_rot13(va_list l)
 {
-	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char lower[] = "abcdefghijklmnopqrstuvwxyz";
-	int i, ctr = 0;
-	char *str = va_arg(l, char*);
+	int i, j, count = 0;
+	char *str, alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
+	str = va_arg(l, char *);
 	if (str == NULL)
-	{
 		str = "(null)";
-		return (-1);
-	}
-	while (*str != '\0')
+	for (i = 0; str[i]; i++)
 	{
-		if (*str >= 'A' && *str <= 'Z')
+		for (j = 0; alph[j]; j++)
 		{
-			for (i = 0; i < 26; i++)
+			if (str[i] == alph[j])
 			{
-				if (upper[i] == *str)
-					break;
+				count += write(1, rot13[j], 1);
+				break;
 			}
-			i += 13;
-			if (i > 26)
-				i = i % 26;
-			write(1, &upper[i], 1);
 		}
-		else if (*str >= 'a' && *str <= 'z')
+		if (!alph[j])
 		{
-			for (i = 0; i < 26; i++)
-				if (lower[i] == *str)
-					break;
-			i += 13;
-			if (i > 26)
-				i = i % 26;
-			write(1, &lower[i], 1);
+			count += write(1, str[i], 1);
 		}
-		else
-			write(1, str, 1);
-		ctr++;
-		str++;
 	}
-	return (ctr);
+	return (count);
 }
