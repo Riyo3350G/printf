a4b86f1cfbdb2de53a9caab6b6cd93ffb16918f6
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-25 01:19:03 -0700

    Update with betty

--- _rot13.c
+++ _rot13.c
@@ -8,27 +8,30 @@
  */
 int print_rot13(va_list l)
 {
-	int i, j, count = 0;
+	int i, j, ctr = 0;
 	char *str, alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	str = va_arg(l, char *);
 	if (str == NULL)
+	{
 		str = "(null)";
+	}
+	
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; alph[j]; j++)
 		{
 			if (str[i] == alph[j])
 			{
-				count += write(1, &rot13[j], 1);
+				ctr += write(1, &rot13[j], 1);
 				break;
 			}
 		}
 		if (!alph[j])
 		{
-			count += write(1, &str[i], 1);
+			ctr += write(1, &str[i], 1);
 		}
 	}
-	return (count);
+	return (ctr);
 }
